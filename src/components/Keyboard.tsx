3e48e788cb29c1c9a739a4678a7726b031c03ad6
Maximilian Paju <maximilian.paju@gmail.com>
2023-07-04 16:19:14 +0200

    chore: improve keyboard layout

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -18,9 +18,7 @@ export function KeyboardLayout({
       return (
         <span
           key={char}
-          onAnimationEnd={() => {
-            console.log("animation ended");
-          }}
+          onAnimationEnd={() => {}}
           className="border px-1 rounded-sm highlight"
         >
           {(renderAs || char).toUpperCase()}
@@ -40,28 +38,30 @@ export function KeyboardLayout({
 
   return (
     <div className="text-sm">
-      <div className="flex gap-2">
-        {["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"].map((char) =>
-          renderKeyboardCharacter(char, highlight)
-        )}
+      <div className="ps-4">
+        <div className="flex gap-2">
+          {["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"].map((char) =>
+            renderKeyboardCharacter(char, highlight)
+          )}
 
-        {renderKeyboardCharacter("⌫", highlight, "←")}
-      </div>
-      <div className="mx-2 my-2 flex gap-2">
-        {["a", "s", "d", "f", "g", "h", "j", "k", "l"].map((char) =>
-          renderKeyboardCharacter(char, highlight)
-        )}
-      </div>
-      <div className="mx-6 my-2 flex gap-2">
-        {["z", "x", "c", "v", "b", "n", "m"].map((char) =>
-          renderKeyboardCharacter(char, highlight)
-        )}
+          {renderKeyboardCharacter("⌫", highlight, "←")}
+        </div>
+        <div className="mx-2 my-2 flex gap-2">
+          {["a", "s", "d", "f", "g", "h", "j", "k", "l"].map((char) =>
+            renderKeyboardCharacter(char, highlight)
+          )}
+        </div>
+        <div className="mx-6 my-2 flex gap-2">
+          {["z", "x", "c", "v", "b", "n", "m"].map((char) =>
+            renderKeyboardCharacter(char, highlight)
+          )}
+        </div>
       </div>
 
       {highlight === "␣" ? (
-        <div className="mx-auto px-10 border border-red-500 w-1/2 h-4"></div>
+        <div className="mx-auto px-10 border border-red-500 w-2/5 h-4"></div>
       ) : (
-        <div className="mx-auto px-10 border border-slate-500 w-1/2 h-4"></div>
+        <div className="mx-auto px-10 border border-slate-500 w-2/5 h-4"></div>
       )}
     </div>
   );
